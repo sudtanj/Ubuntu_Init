f3aa2c6debd4e2ac18deb58874ee140e5e9b34e7
Sudono Tanjung <sudtanj@users.noreply.github.com>
2018-05-27 18:17:21 +0700

    Update init-full.sh

--- us/init-full.sh
+++ us/init-full.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-cat ./sources.list | sudo tee /etc/apt/sources.list
+cat ./sources-full.list | sudo tee /etc/apt/sources.list
 cat ./gpg_keys.txt | sudo tee /etc/apt/gpg_keys.txt
 sudo apt update
